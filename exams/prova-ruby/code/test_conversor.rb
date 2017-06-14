3fb284f00a577be971bbb26a106141f8f458a2d3
Luiz Carlos Rodrigues Chaves <lucachaves@gmail.com>
2017-06-14 15:12:27 -0300

    corrigindo exams/prova-ruby/code/test_conversor.rb

--- exams/prova-ruby/code/test_conversor.rb
+++ exams/prova-ruby/code/test_conversor.rb
@@ -11,10 +11,10 @@ class TestConversor < Minitest::Test
     assert_equal 'ABC', '41 42 43'.hex2str
 
 
-    assert_equal '01000001 01000010', 'AB'.str2hex
-    assert_equal '01000001 01000010 01000011', 'ABC'.str2hex
-    assert_equal 'AB', '41 42'.hex2str
-    assert_equal 'ABC', '41 42 43'.hex2str
+    assert_equal '01000001 01000010', 'AB'.str2bin
+    assert_equal '01000001 01000010 01000011', 'ABC'.str2bin
+    assert_equal 'AB', '01000001 01000010'.bin2str
+    assert_equal 'ABC', '01000001 01000010 01000011'.bin2str
 
   end
 
